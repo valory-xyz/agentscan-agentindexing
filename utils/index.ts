d7c87019ea83b94466c97975e2625fcb9483ef49
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-12-16 14:08:23 -0800

    use correct port

--- utils/index.ts
+++ utils/index.ts
@@ -31,7 +31,7 @@ const axiosInstance = axios.create({
 });
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6378",
+  url: process.env.REDIS_URL || "redis://localhost:6379",
 });
 
 redisClient.connect().catch(console.error);
