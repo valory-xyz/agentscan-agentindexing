df2bc67c25170d4f344fc4aa99eeb98d6f50fc5d
Oscar Perez <ooscar.perez@protonmail.com>
2025-03-25 10:00:07 +0100

    transaction: only process ABI if database is set up

--- src/transactions.ts
+++ src/transactions.ts
@@ -1,18 +1,17 @@
 import { ponder } from "ponder:registry";
-
 import {
   checkAndStoreAbi,
-  convertBigIntsToStrings,
   REGISTER_NAMES,
 } from "../utils";
-
 import { processTransaction } from "../utils/transactionHandling";
+
+const needsAbiTransaction = !process.env.ABI_DATABASE_URL
 
 REGISTER_NAMES.forEach((contractName) => {
   ponder.on(`${contractName}:transaction:from`, ({ event, context }: any) => {
     void processTransaction(event.transaction.hash, event, context, true);
 
-    if (event.transaction.to) {
+    if (needsAbiTransaction && event.transaction.to) {
       void checkAndStoreAbi(
         event.transaction.to.toString(),
         context.network.chainId as any,
@@ -25,7 +24,7 @@ REGISTER_NAMES.forEach((contractName) => {
   ponder.on(`${contractName}:transaction:to`, ({ event, context }: any) => {
     void processTransaction(event.transaction.hash, event, context, false);
 
-    if (event.transaction.from) {
+    if (needsAbiTransaction && event.transaction.from) {
       void checkAndStoreAbi(
         event.transaction.from.toString(),
         context.network.chainId as any,
